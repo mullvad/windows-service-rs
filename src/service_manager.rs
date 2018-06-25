321c9c634c2d2d830c120630e5b51d94e7785126
Andrej Mihajlov <and@mullvad.net>
2018-06-25 13:13:53 +0200

    Add service dependencies

--- src/service_manager.rs
+++ src/service_manager.rs
@@ -1,5 +1,5 @@
 use std::borrow::Cow;
-use std::ffi::OsStr;
+use std::ffi::{OsStr, OsString};
 use std::{io, ptr};
 
 use widestring::{NulError, WideCString, WideString};
@@ -127,6 +127,7 @@ impl ServiceManager {
     ///         error_control: ServiceErrorControl::Normal,
     ///         executable_path: PathBuf::from(r"C:\path\to\my\service.exe"),
     ///         launch_arguments: vec![],
+    ///         dependencies: vec![],
     ///         account_name: None, // run as System
     ///         account_password: None,
     ///     };
@@ -165,6 +166,14 @@ impl ServiceManager {
 
         let launch_command = WideCString::from_wide_str(launch_command_buffer).unwrap();
 
+        let dependency_identifiers: Vec<OsString> = service_info
+            .dependencies
+            .iter()
+            .map(|dependency| dependency.to_system_identifier())
+            .collect();
+        let joined_dependencies = to_double_nul_terminated(&dependency_identifiers)
+            .chain_err(|| ErrorKind::InvalidDependency)?;
+
         let service_handle = unsafe {
             winsvc::CreateServiceW(
                 self.manager_handle.raw_handle(),
@@ -177,7 +186,7 @@ impl ServiceManager {
                 launch_command.as_ptr(),
                 ptr::null(),     // load ordering group
                 ptr::null_mut(), // tag id within the load ordering group
-                ptr::null(),     // service dependencies
+                joined_dependencies.map_or(ptr::null(), |s| s.as_ptr()),
                 account_name.map_or(ptr::null(), |s| s.as_ptr()),
                 account_password.map_or(ptr::null(), |s| s.as_ptr()),
             )
@@ -235,6 +244,30 @@ impl ServiceManager {
 fn to_wide<T: AsRef<OsStr>>(s: Option<T>) -> ::std::result::Result<Option<WideCString>, NulError> {
     if let Some(s) = s {
         Ok(Some(WideCString::from_str(s)?))
+    } else {
+        Ok(None)
+    }
+}
+
+/// A helper to join a collection of `OsStr` into a nul-separated `WideString` ending with two nul
+/// bytes.
+///
+/// Sample output:
+/// "item one\0item two\0\0"
+///
+/// Returns None if the source collection is empty.
+fn to_double_nul_terminated<T: AsRef<OsStr>>(
+    source: &[T],
+) -> ::std::result::Result<Option<WideString>, NulError> {
+    if source.len() > 0 {
+        let mut wide = WideString::new();
+        for s in source {
+            let checked_str = WideCString::from_str(s)?;
+            wide.push_slice(checked_str);
+            wide.push_slice(&[0]);
+        }
+        wide.push_slice(&[0]);
+        Ok(Some(wide))
     } else {
         Ok(None)
     }
