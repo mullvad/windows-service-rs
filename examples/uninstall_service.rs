37146d214c7049ccd201a06e55ac5b44d59a81e3
citreae535 <81646018+citreae535@users.noreply.github.com>
2023-02-07 23:29:51 +0800

    Change `Service.delete` to borrow `self`

--- examples/uninstall_service.rs
+++ examples/uninstall_service.rs
@@ -1,10 +1,10 @@
 #[cfg(windows)]
 fn main() -> windows_service::Result<()> {
-    use std::{thread, time::Duration};
     use windows_service::{
         service::{ServiceAccess, ServiceState},
         service_manager::{ServiceManager, ServiceManagerAccess},
     };
+    use windows_sys::Win32::Foundation::ERROR_SERVICE_DOES_NOT_EXIST;
 
     let manager_access = ServiceManagerAccess::CONNECT;
     let service_manager = ServiceManager::local_computer(None::<&str>, manager_access)?;
@@ -12,14 +12,26 @@ fn main() -> windows_service::Result<()> {
     let service_access = ServiceAccess::QUERY_STATUS | ServiceAccess::STOP | ServiceAccess::DELETE;
     let service = service_manager.open_service("ping_service", service_access)?;
 
-    let service_status = service.query_status()?;
-    if service_status.current_state != ServiceState::Stopped {
+    // The service will be marked for deletion as long as this function call succeeds.
+    // However, it will not be deleted from the database until it is stopped and all open handles to it are closed.
+    service.delete()?;
+    // Our handle to it is not closed yet. So we can still query it.
+    if service.query_status()?.current_state != ServiceState::Stopped {
+        // If the service cannot be stopped, it will be deleted when the system restarts.
         service.stop()?;
-        // Wait for service to stop
-        thread::sleep(Duration::from_secs(1));
+    }
+    // Explicitly close our open handle to the service. This is automatically called when `service` goes out of scope.
+    drop(service);
+
+    // Check if the service is deleted.
+    if let Err(windows_service::Error::Winapi(e)) =
+        service_manager.open_service("ping_service", ServiceAccess::QUERY_STATUS)
+    {
+        if e.raw_os_error() == Some(ERROR_SERVICE_DOES_NOT_EXIST as i32) {
+            println!("ping_service is deleted.");
+        }
     }
 
-    service.delete()?;
     Ok(())
 }
 
