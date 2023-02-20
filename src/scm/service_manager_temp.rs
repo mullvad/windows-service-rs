4cd08565246e66386b8adec38e651d325ada535b
citreae535 <81646018+citreae535@users.noreply.github.com>
2023-02-20 20:14:22 +0800

    Remove flags

--- src/scm/service_manager_temp.rs
+++ src/scm/service_manager_temp.rs
@@ -8,20 +8,6 @@ use crate::sc_handle::ScHandle;
 use crate::service::{to_wide, RawServiceInfo, Service, ServiceAccess, ServiceInfo};
 use crate::{Error, Result};
 
-bitflags::bitflags! {
-    /// Flags describing access permissions for [`ServiceManager`].
-    pub struct ServiceManagerAccess: u32 {
-        /// Can connect to service control manager.
-        const CONNECT = Services::SC_MANAGER_CONNECT;
-
-        /// Can create services.
-        const CREATE_SERVICE = Services::SC_MANAGER_CREATE_SERVICE;
-
-        /// Can enumerate services or receive notifications.
-        const ENUMERATE_SERVICE = Services::SC_MANAGER_ENUMERATE_SERVICE;
-    }
-}
-
 /// Service manager.
 pub struct ServiceManager {
     manager_handle: ScHandle,
